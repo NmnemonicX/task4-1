b071c850f5ad8d3345fa072277f70f778f1b0157
NmnemoniX <yzhakovng@rambler.ru>
2021-12-06 23:45:21 +0500

    two

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ const yargs = require('yargs/yargs')
 const {hideBin} = require('yargs/helpers')
 const fs = require('fs')
 const path = require('path')
-
+const  readline = require('readline')
+const input = readline.createInterface(process.stdin , process.stdout)
 
 const argv1 = yargs(hideBin(process.argv))
     .option('file', {
@@ -15,16 +16,49 @@ const pathexist = path.join(__dirname, argv1.f)
 
  if(!fs.existsSync(pathexist))
  {
-     fs.writeFileSync(pathexist,'','utf8')
+     fs.writeFileSync(pathexist,'first','utf8')
  }
+
+ const firstData = fs.readFileSync(pathexist,'utf8')
+console.log(firstData)
 
 let   targetNumber = (Math.random()<.5)+1;
 console.log('число для угадывания:',targetNumber)
 
+const stream = fs.createWriteStream(
+    pathexist,
+    'utf8'
+);
+stream.on('error', (err) => console.log(`Err: ${err}`));
+stream.on('finish', () => console.log('Done'));
+stream.write(firstData);
+
 function game() {
     console.log(targetNumber)
-
+    input.question( 'введите 1 или 2',(luckyNumber) =>{
+        console.log('luckyNumber',luckyNumber)
+        console.log('targetNumber',targetNumber)
+        console.log('luckyNumber!=1',luckyNumber!=1)
+        console.log('luckyNumber!=2',luckyNumber!=2)
+        if(luckyNumber!=1 && luckyNumber!=2){
+            console.log(' ввели не подходящие числа, попробуите еще раз')
+            game()
+        }
+        else {
+            if (luckyNumber == targetNumber) {
+                console.log('вы угадали!')
+                stream.write('Угадал\n');
+            } else {
+                console.log('вы  не угадали!')
+                stream.write('Не Угадал\n');
+            }
+            stream.end();
+            input.close()
+        }
+    })
 }
 
 
 game()
+//
+console.log('конец')
